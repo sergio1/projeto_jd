7e69e0113a2d7f7a938e93ad0302cc012b446b7d
Sergio MS <sergiombel@gmail.com>
2025-06-16 21:05:54 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 34
+nome  = "Luciano"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista =[1,2,3,4,5]
+print(lista)
+
+# Métodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
+
