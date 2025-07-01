efdb8fcf1ff7d418e18c8b0d52e9b9af1fb939d4
Sergio MS <sergiombel@gmail.com>
2025-07-01 14:37:20 -0300

    primeiros exemplos

---
+++ bootcamp/exemplo_01.py
@@ -0,0 +1,3 @@
+# crie programa que o usuário digite o seu nome e retorna o número de caracteres do nome
+nome = input('Digite o seu nome: ')
+print('Quantidade de caracteres:' + str(len(nome)))
---
+++ bootcamp/exemplo_02.py
@@ -0,0 +1,5 @@
+# crie programa onde o usuário digite dois valores e apareça a soma.
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+soma  = valor1 + valor2
+print('Soma :' + str(soma))
