5ddb04c1b41e21ec90cb8a62ac4a061599159936
Sergio MS <sergiombel@gmail.com>
2025-07-03 16:08:15 -0300

    segunda aula

---
+++ bootcamp/aula02/main.py
@@ -0,0 +1,7 @@
+numero = input("Inserir um número: ")
+if isinstance(numero, int):
+    print(f"O número {numero} é um inteiro")
+#elif isinstance(numero, float):
+
+else: 
+    print(f"A variável não é um número válido")
